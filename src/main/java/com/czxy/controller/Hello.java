e70f80e4ec04340052cca34d2061b70235e93fed
MSI-PC <zhengdong0621@qq.com>
2019-07-29 08:37:19 +0800

    提交

---
+++ src/main/java/com/czxy/controller/Hello.java
@@ -0,0 +1,18 @@
+package com.czxy.controller;
+
+/**
+ * @author MSI-PC
+ * @version v 1.0
+ * @date 2019/7/29
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("海");
+        System.out.println("海");
+        System.out.println("海");
+        System.out.println("海");
+        System.out.println("海");
+    }
+}
